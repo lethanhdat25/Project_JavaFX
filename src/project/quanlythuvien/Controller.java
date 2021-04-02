d7e9a5113464590367432a7257e33564b34d8e5f
Manh2362000 <70956659+Manh2362000@users.noreply.github.com>
2021-04-02 10:32:59 +0700

    add

---
+++ src/project/quanlythuvien/Controller.java
@@ -0,0 +1,17 @@
+package project.quanlythuvien;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public final static String connectString="jdbc:mysql://localhost:3306/T2008M";
+    public final static String user="root";
+    public final static String password="";
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
